e8185cdcd1948753b36cd72a956cfdcfdd84645f
Vlad Bogodyazh <liquid98@yandex.by>
2024-12-03 11:42:45 +0300

    add configs webpack/typescript/eslint/prettier, create & add first component

---
+++ webpack.config.ts
@@ -0,0 +1,90 @@
+import webpack, { Configuration } from 'webpack';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+
+interface CustomConfiguration extends Configuration {
+    devServer: DevServerConfiguration;
+}
+
+type ConfigMode = 'production' | 'development';
+
+interface ConfigEnv {
+    mode: ConfigMode;
+    port: number;
+}
+
+export default (env: ConfigEnv): CustomConfiguration => {
+    const mode = env.mode || 'development';
+    const PORT = env.port || 3000;
+
+    const isDev = mode === 'development';
+
+    return {
+        mode,
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        output: {
+            filename: '[name].[contenthash].js',
+            path: path.resolve(__dirname, 'build'),
+            clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'public', 'index.html'),
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css',
+            }),
+            new webpack.HotModuleReplacementPlugin(),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.svg$/,
+                    use: ['@svgr/webpack'],
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    auto: (resPath: string) =>
+                                        Boolean(resPath.includes('.module.')),
+                                    localIdentName: isDev
+                                        ? '[path][name]__[local]--[hash:base64:5]'
+                                        : '[hash:base64:8]',
+                                },
+                            },
+                        },
+                        'sass-loader',
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+            preferAbsolute: true,
+            modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+            mainFiles: ['index'],
+            alias: { '@': path.resolve(__dirname, 'src') },
+        },
+        devServer: isDev
+            ? {
+                  port: PORT,
+                  open: true,
+                  historyApiFallback: true,
+                  hot: true,
+              }
+            : undefined,
+    };
+};
